497637d2ccbc11df72c59f07abcc30a543d565ea
BenjaminChan <benjamin.v.chan@gmail.com>
2025-02-25 22:06:37 -0800

    02_sample_means script outline

---
+++ scripts/02_sample_means.py
@@ -0,0 +1,8 @@
+# Import necessary libraries
+# Load configuration parameters (sample size, number of samples)
+# Read the generated population dataset
+# Perform repeated sampling:
+#    - Randomly select a sample from the population
+#    - Compute the mean of the sample
+#    - Store the sample mean
+# Save the computed sample means to a CSV file in outputs/
