1e07c3e632c08e515a7bc815340663733420c1bf
BenjaminChan <benjamin.v.chan@gmail.com>
2025-02-25 22:13:15 -0800

    config.py complete

---
+++ config.py
@@ -0,0 +1,9 @@
+POPULATION_SIZE = 100000
+SAMPLE_SIZE = 50
+NUM_SAMPLES = 1000
+
+DISTRIBUTION = "exponential"  # Options: "uniform", "exponential", "poisson"
+PARAM_LOW = 0
+PARAM_HIGH = 100
+PARAM_SCALE = 10  # For exponential
+PARAM_LAMBDA = 5  # For Poisson
