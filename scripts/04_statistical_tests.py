95cbd6c4e8d0834e94e130389e89a0563efc43ce
BenjaminChan <benjamin.v.chan@gmail.com>
2025-02-25 22:07:01 -0800

    04_statistical_tests script outline

---
+++ scripts/04_statistical_tests.py
@@ -0,0 +1,7 @@
+# Import necessary libraries
+# Load sample means dataset
+# Perform normality tests:
+#    - Shapiro-Wilk test
+#    - Kolmogorov-Smirnov test
+#    - Anderson-Darling test
+# Store test results in a text file inside outputs/
