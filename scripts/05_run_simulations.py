d26eed3c87a873e41640bf0d5b8eca3c2184b737
BenjaminChan <benjamin.v.chan@gmail.com>
2025-02-25 22:07:18 -0800

    05_run_simulations script outline

---
+++ scripts/05_run_simulations.py
@@ -0,0 +1,7 @@
+# Import necessary functions from other scripts
+# Execute the scripts sequentially:
+#    - Generate population
+#    - Compute sample means
+#    - Analyze and visualize sample mean distribution
+#    - Perform statistical tests
+# Print/log results to the console
